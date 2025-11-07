fdf8e7176162c605aa1b13114ca1e8fd34ee0e78
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-08 01:03:47 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.EvdokimovKP.Sprint4.Task0.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task0.V1.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint4.Task0.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] numsArrey = { 6, 4, 3, 2, 1, 0, 9, 8, 7, 5 };
+            int res = ds.GetSumEvenArrEl(numsArrey);
+            int wait = 20;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint4.Task0.V1/Program.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task0.V1.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("********************************************************************");
+Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:                                                    ");
+Console.WriteLine("********************************************************************");
+int[] numsArrey = { 6,4, 3, 2, 1, 0, 9, 8, 7, 5 };
+Console.WriteLine("Массив:");
+for (int i = 0; i < numsArrey.Length; i++)
+{
+    Console.WriteLine(numsArrey[i]);
+}
+Console.WriteLine("********************************************************************");
+Console.WriteLine("РЕЗУЛЬТАТ:                                                          ");
+Console.WriteLine("********************************************************************");
+Console.WriteLine(ds.GetSumEvenArrEl(numsArrey));
+Console.ReadKey();
