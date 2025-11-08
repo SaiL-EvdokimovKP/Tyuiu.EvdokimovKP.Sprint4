0f624f0e6957f2a62278bab9db53cf888664cc0a
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-08 22:56:42 +0500

    Task1.V4

---
+++ Tyuiu.EvdokimovKP.Sprint4.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task1.V4.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint4.Task1.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[] array = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            int res = ds.Calculate(array);
+            int wait = 945;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint4.Task1.V4/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task1.V4.Lib;
+DataService ds = new DataService();
+Console.WriteLine("********************************************************************");
+Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:                                                    ");
+Console.WriteLine("********************************************************************");
+int dlina;
+Console.WriteLine("Введите длинну массива: ");
+dlina = Convert.ToInt32(Console.ReadLine());
+
+int[] Array = new int[dlina];
+
+for (int i = 0; i <= dlina - 1; i++)
+{
+    Console.WriteLine("Введите значение " + i + " элемент массива: ");
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine();
+Console.WriteLine("Массив: ");
+for (int i = 0; i <= dlina - 1; i++)
+{
+    Console.WriteLine(Array[i]);
+}
+Console.WriteLine();
+Console.WriteLine("********************************************************************");
+Console.WriteLine("РЕШЕНИЕ:                                                            ");
+Console.WriteLine("********************************************************************");
+int res = ds.Calculate(Array);
+Console.WriteLine(res);
