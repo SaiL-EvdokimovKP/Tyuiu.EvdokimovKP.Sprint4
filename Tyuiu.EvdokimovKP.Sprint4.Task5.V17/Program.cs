a513272460275b2a700c4ae3d1e84bf69a4cbb0b
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-10 04:16:37 +0500

    Task5.V17

--- Tyuiu.EvdokimovKP.Sprint4.Task5.V17/Program.cs
+++ Tyuiu.EvdokimovKP.Sprint4.Task5.V17/Program.cs
@@ -8,18 +8,18 @@ int str = 5;
 int stolb = 5;
 int[,] matrix = new int[str, stolb];
 
-for (int i = 0; i <= str; i++)
+for (int i = 0; i < str; i++)
 {
-    for (int j = 0; j <= stolb; j++)
+    for (int j = 0; j < stolb; j++)
     {
         matrix[i, j] = random.Next(-9, 4);
     }
 }
 Console.WriteLine("********************************************************************");
 Console.WriteLine("\nМассив");
-for (int i = 0; i <= str; i++)
+for (int i = 0; i < str; i++)
 {
-    for (int j = 0; j <= stolb; j++)
+    for (int j = 0; j < stolb; j++)
     {
         Console.Write($"{matrix[i, j]} \t");
     }
