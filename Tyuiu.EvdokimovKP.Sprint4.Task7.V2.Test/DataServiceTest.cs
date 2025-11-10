3385ad4039b3017e5d24cd36c3ac3c066b0abac9
SaiL-EvdokimovKP <evdokimovkirill373@gmail.com>
2025-11-10 06:43:30 +0500

    Task7.V2

---
+++ Tyuiu.EvdokimovKP.Sprint4.Task7.V2.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task7.V2.Lib;
+
+namespace Tyuiu.EvdokimovKP.Sprint4.Task7.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "597643158942";
+            DataService ds = new DataService();
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 6144;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.EvdokimovKP.Sprint4.Task7.V2/Program.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.EvdokimovKP.Sprint4.Task7.V2.Lib;
+DataService ds = new DataService();
+int index = 0;
+int n = 5;
+int m = 3;
+int[,] mtrx = new int[n, m];
+string str = "597643158942";
+
+Console.WriteLine("********************************************************************");
+Console.WriteLine("ИСХОДНЫЕ ДАННЫЕ:                                                    ");
+Console.WriteLine("********************************************************************");
+Console.WriteLine("\nМассив:");
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        Console.Write($"{str[index]} \t");
+        index++;
+    }
+    Console.WriteLine();
+}
+Console.WriteLine("********************************************************************");
+Console.WriteLine("РЕШЕНИЕ:                                                            ");
+Console.WriteLine("********************************************************************");
+int res = ds.Calculate(n, m, str);
+Console.WriteLine("Произведение четных элементов = " + res);
+
+Console.ReadLine();
